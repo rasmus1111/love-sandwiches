ffc65e28370b938951bf67ba4096e3428927543a
rasmus1111 <rasmusareaxelsson@gmail.com>
2023-02-07 12:40:17 +0000

    extra commit

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
